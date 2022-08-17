e34a7b438f40f69a8553b1b543b0d30f088859de
tayyjohnson <taydriver1@gmail.com>
2022-08-17 12:02:00 -0400

    fixed lines in 16 & 19 for resub

--- server.js
+++ server.js
@@ -13,10 +13,10 @@ app.get('/api/notes', (req, res) => {
 });
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "public/index.html"))
+    res.sendFile(path.join(__dirname, "public/assets/index.html"))
 });
 app.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "public/notes.html"))
+    res.sendFile(path.join(__dirname, "public/assets/notes.html"))
 });
 
 app.post("/api/notes", (req, res) => {
