2326c98ef389c4ef1109c97850eda1055e49b6f0
tayyjohnson <taydriver1@gmail.com>
2022-08-18 09:39:35 -0400

    fixed? maybe?

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.get('/api/notes', (req, res) => {
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "public/assets/index.html"))
 });
-app.get("/notes", (req, res) => {
+app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "public/assets/notes.html"))
 });
 
