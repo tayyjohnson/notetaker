cede9432c04be7734771ef15ca16fbf45047fcd8
tayyjohnson <taydriver1@gmail.com>
2022-08-18 10:57:04 -0400

    fiddled with server.js file

--- server.js
+++ server.js
@@ -8,18 +8,18 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get('/api/notes', (req, res) => {
+app.get('/notes', (req, res) => {
     res.json(data);
 });
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "public/assets/index.html"))
+    res.sendFile(path.join(__dirname, "public/index.html"))
 });
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "public/assets/notes.html"))
+    res.sendFile(path.join(__dirname, "public/notes.html"))
 });
 
-app.post("/api/notes", (req, res) => {
+app.post("/notes", (req, res) => {
     data.push(req.body);
     for (i=0; i<data.length; i++) {
         data[i].id = i+1;
@@ -32,7 +32,7 @@ app.post("/api/notes", (req, res) => {
 
 });
 
-app.delete("/api/notes/:id", (req, res) =>{
+app.delete("/notes/:id", (req, res) =>{
     let index = req.params.id - 1;
     newArr = data.splice(index, 1);
     for (i=0; i<newArr.length; i++) {
