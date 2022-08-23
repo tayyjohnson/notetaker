f90be148e08c73b9fbaa2a43749edc8f327ba27a
tayyjohnson <taydriver1@gmail.com>
2022-08-23 19:33:31 -0400

    fixed app & it deploys properly

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.get('/api/notes', (req, res) => {
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "public/index.html"))
 });
-app.get("/", (req, res) => {
+app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "public/notes.html"))
 });
 
